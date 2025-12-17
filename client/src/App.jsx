a9c2674d409731427f640cc86c5fa26c1673a502
francis <clyderitchie@yahoo.com>
2025-12-17 15:03:30 -0800

    Login page working

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,17 +1,49 @@
-import React from "react";
-import { ApolloProvider } from "@apollo/client";
+import React, { useState } from "react";
+import {
+  ApolloClient,
+  ApolloProvider,
+  createHttpLink,
+  InMemoryCache,
+} from "@apollo/client";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { setContext } from "@apollo/client/link/context";
 
 import Home from "./pages/home";
 import NavBar from "./components/NavBar.jsx";
 import PersonalPage from "./pages/personal.jsx";
 import Blog from "./pages/blog.jsx";
 import CreateBlogForm from "./components/createBlog.jsx";
+import LoginPage from "./pages/login.jsx";
+
+const httpLink = createHttpLink({
+  uri: "/graphql",
+});
+
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem("token");
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : "",
+    },
+  };
+});
+
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
 
 function App() {
+    const [isLoggedIn, setIsLoggedIn] = useState(!!localStorage.getItem('token'));
+
+  const handleLogin = () => {
+    setIsLoggedIn(true);
+  };
+
   return (
     <>
-      <ApolloProvider>
+      <ApolloProvider client={client}>
         <NavBar />
         <Router>
           <Routes>
@@ -19,6 +51,10 @@ function App() {
             <Route path="/personal" element={<PersonalPage />} />
             <Route path="/blog" element={<Blog />} />
             <Route path="/createBlog" element={<CreateBlogForm />} />
+            <Route
+              path="/login"
+              element={<LoginPage handleLogin={handleLogin} />}
+            />
           </Routes>
         </Router>
       </ApolloProvider>
