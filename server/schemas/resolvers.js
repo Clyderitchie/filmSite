bfb57898f1bdfb40669a3804a2544c3def44d980
francis <clyderitchie@yahoo.com>
2025-12-17 20:58:54 -0800

    Updated server

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -18,6 +18,14 @@ module.exports = {
     getPost: async (_, args) => {
       return await Post.findById(args.postId).populate("userId");
     },
+    me: async (_, __, context) => {
+      if (context.user) {
+        return await User.findById(context.user._id).populate({
+          path: "posts",
+          populate: { path: "userId" },
+        });
+      }
+    },
   },
   Mutation: {
     createUser: async (_, args) => {
@@ -30,10 +38,10 @@ module.exports = {
       if (context.user) {
         const post = await Post.create({
           ...args,
-          userId: useContext.user._id,
+          userId: context.user._id,
         });
         await User.findByIdAndUpdate(
-          useContext.user._id,
+          context.user._id,
           { $push: { posts: post._id } },
           { new: true }
         );
@@ -41,14 +49,14 @@ module.exports = {
       }
       throw AuthenticationError;
     },
-    login: async (_, {username, password }) => {
+    login: async (_, { username, password }) => {
       const user = await User.findOne({ username });
 
       if (!user) {
         throw AuthenticationError;
       }
 
-      const correctPw = await user.isCorrectPassword(password)
+      const correctPw = await user.isCorrectPassword(password);
 
       if (!correctPw) {
         throw AuthenticationError;
@@ -56,6 +64,40 @@ module.exports = {
 
       const token = signToken(user);
       return { token, user };
-    }
+    },
+    deletePost: async (_, { postId }) => {
+      console.log("Received the postId to delete: ", postId);
+
+      try {
+        const post = await Post.findById(postId);
+
+        if (!post) {
+          throw new Error("Post cannot be found.");
+        }
+
+        const deletePost = await Post.findByIdAndDelete(postId);
+
+        return true;
+      } catch (error) {
+        console.error("Delete post error: ", error);
+      }
+    },
+    deleteUser: async (_, { userId }) => {
+      console.log("Received the userId to delete: ", userId);
+
+      try {
+        const user = User.findById(userId);
+
+        if (!user) {
+          throw new Error("User cannot be found ");
+        }
+
+        const deleteUser = await User.findByIdAndDelete(userId);
+
+        return true;
+      } catch (error) {
+        console.error("Error with deleting user: ", error);
+      }
+    },
   },
 };
